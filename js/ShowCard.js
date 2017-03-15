0f17d26a226c72e8a71184e8cbc28d2c6a09f717
Tomos John Rees <sabt@live.co.uk>
2017-03-15 19:56:48 +0000

    Add propTypes to ShowCard

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,6 +1,20 @@
 import React from 'react'
+const { shape, string } = React.PropTypes
 
 const ShowCard = React.createClass({
+  // propTypes are optional. Helpful for people to instantly see
+  // the contract i.e. what they need to pass the component
+  // to use it.
+
+  // React only checks propTypes in dev
+  propTypes: {
+    show: shape({
+      poster: string,
+      title: string,
+      year: string,
+      description: string
+    })
+  },
   render () {
     // destucturing allows a much cleaner component
     const { poster, title, year, description } = this.props.show
