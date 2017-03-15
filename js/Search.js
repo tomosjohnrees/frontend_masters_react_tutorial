ec0115debefc3bc052c87f0a9b088c8d047dfaa9
Tomos John Rees <sabt@live.co.uk>
2017-03-15 17:43:40 +0000

    Add landing and search page with react-router HashRouter and Match

---
+++ js/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+console.log("oi oi");
+const Search = React.createClass({
+  render () {
+    return (
+      <h1> Search page</h1>
+    )
+  }
+})
+
+export default Search
