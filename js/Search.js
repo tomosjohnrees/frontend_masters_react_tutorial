56d9eebe6b848df779eea77fad056ea46a17d14e
Tomos John Rees <sabt@live.co.uk>
2017-03-15 21:18:25 +0000

    Add searchTerm state to search component

--- js/Search.js
+++ js/Search.js
@@ -5,13 +5,23 @@ import ShowCard from './ShowCard'
 // Spread opperator
 // {...show} is equvalent to title={show.title} something={sow.something}
 
+// React doesn't have two way data binding, hence we need the onChange on the input value
+
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default string'
+    }
+  },
+  handleSearchValueChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className="search">
         <header>
           <h1>svideo</h1>
-          <input type='text' placeholder='Search' />
+          <input onChange={this.handleSearchValueChange}  value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
           {preLoad.shows.map( show => {
